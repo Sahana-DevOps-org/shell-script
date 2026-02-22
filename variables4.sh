cba71d7a352c38d4677da444ce511ede078b888b
sahana1644 <lakkireddysahana164@gmail.com>
2026-02-22 15:23:58 +0530

    variables

---
+++ variables4.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "all args passed to the script : $@"
+echo "number of variables passed : $#"
+echo "script name :$0"
+echo "present directory : $PWD"
+echo "who is running: $USER"
+echo "home directory of current user: $home"
+echo "PID of the script:$$"
+sleep 100 &
+echo "PID of recently excecuted background process: $!"
+echo "all args passed to the script : $*"
