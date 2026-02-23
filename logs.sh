70e7b5c670252d8e01ff0e898b4b37f1d2dbd3d1
sahana1644 <lakkireddysahana164@gmail.com>
2026-02-23 11:53:44 +0530

    script

---
+++ logs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+USERID=$(id -u)
+LOGS_FOLDER="var/log/shell-script"
+
+if [ $USERID -ne 0 ]; then
+    echo "Please run this script with rootuser access"
+    exit 1
+fi
+VALIDATE (){
+    if [ $1 -ne 0 ]; then
+        echo "installing $2 failure"
+        exit 1
+    else 
+        echo "installing $2 is success"
+    fi
+}
+echo "installing nginx"
+dnf install nginx -y
+VALIDATE $? "Installing nginx"
+dnf install mysql -y
+VALIDATE $? "Installing mysql"
