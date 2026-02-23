8b388bf0793e56f7668c6ab06a5a82ed1a9ae90e
sahana1644 <lakkireddysahana164@gmail.com>
2026-02-23 13:17:21 +0530

    script

--- logs.sh
+++ logs.sh
@@ -4,16 +4,16 @@ LOGS_FOLDER="var/log/shell-script"
 LOGS_FILE="var/log/shell-script/$0.log"
 
 if [ $USERID -ne 0 ]; then
-    echo "Please run this script with rootuser access"
+    echo "Please run this script with rootuser access" | tee -a $LOGS_FILE
     exit 1
 fi
 mkdir -p $LOGS_FOLDER
 VALIDATE (){
     if [ $1 -ne 0 ]; then
-        echo "installing $2 failure"
+        echo "installing $2 failure" | tee -a $LOGS_FILE
         exit 1
     else 
-        echo "installing $2 is success"
+        echo "installing $2 is success" | tee -a $LOGS_FILE
     fi
 }
 echo "installing nginx"
