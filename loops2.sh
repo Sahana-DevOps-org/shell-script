e054f3619053de7d1601559a13ee5f04b5339198
sahana1644 <lakkireddysahana164@gmail.com>
2026-02-23 14:36:07 +0530

    loops

--- loops2.sh
+++ loops2.sh
@@ -17,13 +17,12 @@ VALIDATE (){
 }    
 for package in $@
 do 
-    dnf install $package &>> $LOGS_FILE
+    dnf list installed $package &>> $LOGS_FILE
     if [ $? -ne 0 ]; then
         echo "$package is not installed,installing now"
         dnf install $package -y &>> $LOGS_FILE
-        VALIDATE (){$? "$package installation"
+        VALIDATE $? "$package installation"
     else 
         echo "$package already installed skipping"
     fi
-        }
 done
