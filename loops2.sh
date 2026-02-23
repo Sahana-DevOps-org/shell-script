0018c793c66060bc637007cab1aa422ecd95c571
sahana1644 <lakkireddysahana164@gmail.com>
2026-02-23 14:26:16 +0530

    loops

---
+++ loops2.sh
@@ -0,0 +1,25 @@
+USERID=$(id -u)
+LOGS_FOLDER="var/log/shell-script"
+LOGS_FILE="var/log/shell-script/$0.log"
+
+if [ $USERID -ne 0 ]; then
+    echo "Please run this script with rootuser access" | tee -a $LOGS_FILE
+    exit 1
+fi
+mkdir -p $LOGS_FOLDER
+VALIDATE (){
+    if [ $1 -ne 0 ]; then
+        echo "installing $2 failure" | tee -a $LOGS_FILE
+        exit 1
+    else 
+        echo "installing $2 is success" | tee -a $LOGS_FILE
+    fi
+}    
+for package in $@
+do 
+    dnf install $package &>> $LOGS_FILE
+    if [ $? -ne 0 ]; then
+    echo "$package is not installed,installing now"
+    dnf install $package -y &>> $LOGS_FILE
+    VALIDATE $? "$package installation"
+done
