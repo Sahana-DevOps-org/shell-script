6902d449bce53a7a0354979f3b265bb5017a08de
sahana1644 <lakkireddysahana164@gmail.com>
2026-02-23 11:33:06 +0530

    script

--- functions.sh
+++ functions.sh
@@ -5,14 +5,14 @@ if [ $USERID -ne 0 ]; then
     echo "Please run this script with rootuser access"
     exit 1
 fi
-VALIDATE (
+VALIDATE (){
     if [ $1 -ne 0 ]; then
         echo "installing $2 failure"
         exit 1
     else 
         echo "installing $2 is success"
     fi
-)
+}
 echo "installing nginx"
 dnf install nginx -y
 VALIDATE $? "Installing nginx"
